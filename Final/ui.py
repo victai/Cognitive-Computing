1f23da05a26d88edbf32caeef7dd8eef1f1a7ec5
victai <b04902105@ntu.edu.tw>
2018-12-19 21:43:02 +0800

    use listbox; rearange position

--- Final/ui.py
+++ Final/ui.py
@@ -22,55 +22,41 @@ def choosepic():
 def predict():
     print('prdeict')
 
-chosen_taglist = []
-
 def construct_gui():
     global window
     window = tk.Tk()
     window.title('Cognitive Computing Final Demo')
-    window.geometry('1000x500')
-    global path; global e1; global l1;
+    window.geometry('1000x800')
+    global path; global l1;
     path=StringVar()  
     Button(window,text='upload image',command=choosepic).place(x=120,y=5)
     Button(window,text='predict image',command=predict).place(x=250,y=5)  
     l1=Label(window)  
     l1.place(x=10,y=60)
 
-    tag = tk.StringVar()
-    l = tk.Label(window, 
-                textvariable=tag,
-                bg='white',     
-                font=('Arial', 15),     
-                width=15, height=2
-        )
-    l.place(x=743,y = 10)
 
-
-    
     chosen_var = tk.StringVar(window)
-    chosen_var.set("null")
+    chosen_var.set("")
     w = OptionMenu(window, chosen_var, *opt)
-    w.place(x=802,y=60)
+    w.place(x=800,y=10)
 
-    picked_tag = tk.StringVar()
+    var = tk.StringVar()
+    picked_taglist = tk.Listbox(window, listvariable=var)
+    picked_taglist.place(x=600,y=10)
+
+    ## pick a word and add to list
     def add_to_list():
         tmp = chosen_var.get()
-        chosen_taglist.append(tmp)
-        picked_tag.set(','.join(chosen_taglist))
+        picked_taglist.insert('end', tmp)
+        chosen_var.set("")
 
-    picked = tk.Label(window,
-                     textvariable=picked_tag,
-                     bg='white',
-                     font=('Arial', 15),
-                     width=50, height=2)
-    picked.place(x=20,y=100)
+    button = Button(window, text="Add", command=add_to_list)
+    button.place(x=900,y=10)
+    #####
 
-    button = Button(window, text="OK", command=add_to_list)
-    button.place(x=802,y=100)
-    
     window.mainloop()
 
 
 if __name__ == '__main__':
-    opt = [*range(10)]
+    opt = np.random.choice(np.arange(1000), 10)
     construct_gui()
